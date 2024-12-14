164676f1b29543e6a1dd8d40d9b8e91f96efa417
Fukayama <129649006+SFukayama@users.noreply.github.com>
2024-12-14 17:04:09 +0900

    Add files via upload

---
+++ LunchLog/src/main/java/com/example/LunchLog/LunchLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.LunchLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchLogApplication.class, args);
+	}
+
+}
